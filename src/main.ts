e3646bd7a03f2efff85f0d2a7c82b6dbd39e7064
Samim Umut Ökten <samim.oekten@unifr.ch>
2024-11-24 17:23:02 +0100

    Add routing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Aurelia from 'aurelia';
 import { MyApp } from './my-app';
 import { AllConfiguration } from '@aurelia-mdc-web/all';
+import { RouterConfiguration } from '@aurelia/router';
 
 Aurelia
   .register(AllConfiguration)
+  .register(RouterConfiguration)
   .app(MyApp)
   .start();
--- src/my-app.ts
+++ src/my-app.ts
@@ -1,3 +1,19 @@
-export class MyApp {
-  public message = 'Hello World!';
+import { IRouteableComponent, IRoute } from '@aurelia/router';
+
+export class MyApp implements IRouteableComponent {
+    static routes: IRoute[] = [
+        {
+            path: '',
+            component: () => import('./views/dashboard'),
+            id: 'Dashboard',
+            title: 'Dashboard'
+        },
+        {
+            path: 'about',
+            component: () => import('./views/about'),
+            id: 'About',
+            title: 'About'
+        },
+    ];
+
 }
