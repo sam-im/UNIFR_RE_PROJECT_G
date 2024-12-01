7ae76a4d6a473f4a2f832d5c3d1e057fb74512dd
Samim Umut Ökten <samim.oekten@unifr.ch>
2024-12-01 15:18:15 +0100

    Style top-level nav

--- src/resources/auth-hook.ts
+++ src/resources/auth-hook.ts
@@ -10,7 +10,7 @@ export class AuthHook implements ILifecycleHooks {
 
     canLoad() {
         if ( !this.auth.isLoggedIn ) {
-            return 'login';
+            return false;
         }
 
         return true;
