a3ab6f992135f2aaf6f0c7ea049d4f7e77a7994a
Samim Umut Ökten <samim.oekten@unifr.ch>
2024-12-03 17:10:23 +0100

    Only show my-courses, my-faculty to students

--- src/views/dashboard.ts
+++ src/views/dashboard.ts
@@ -1,5 +1,10 @@
+import { IAuthService } from '../resources/auth-service';
 import { AuthHook } from "../resources/auth-hook";
+import { inject } from 'aurelia';
 
+@inject(IAuthService)
 export class Dashboard {
     static dependencies = [AuthHook];
+
+    constructor(private auth: IAuthService) {}
 }
