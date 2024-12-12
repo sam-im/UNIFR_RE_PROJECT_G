081d9918e9c8fd011d5237c5b33269d89da0b0d6
Samim Umut Ökten <samim.oekten@unifr.ch>
2024-12-12 21:18:46 +0100

    Implement rest of the register view

--- src/views/administrator/register.ts
+++ src/views/administrator/register.ts
@@ -1,1 +1,11 @@
-export class Register {}
+export class Register {
+    firstName = '';
+    lastName = '';
+
+    get emailPreview() {
+        if (this.firstName !== '' && this.lastName !== '') {
+            return `${this.firstName.toLowerCase()}.${this.lastName.toLowerCase()}@domain.tld`;
+        }
+        return '';
+  }
+}
